bc5c1dd7f9b3497aa11b9aa50feda8dfe8691f64
James Lockhart <james.lockhart117@gmail.com>
2017-12-20 16:37:09 +0300

    add try/catch features

--- Fitness/Program.cs
+++ Fitness/Program.cs
@@ -6,57 +6,62 @@ namespace Fitness
     {
         static void Main(string[] args)
         {
-            int runningTotal = 0;
-            bool keepGoing = true;
+            var runningTotal = 0.0;
 
-            while (keepGoing)
+            while (true)
             {
                 // Prompt the user for minutes exercised
                 Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
-                string entry = Console.ReadLine();
+                var entry = Console.ReadLine();
 
-                if (entry == "quit")
+                if (entry.ToLower() == "quit")
                 {
-                    keepGoing = false;
+                    break;
                 }
                 else
                 {
-                    int minutes = int.Parse(entry);
-
-                    if (minutes <= 0)
+                    
+                    try
                     {
-                        Console.WriteLine(minutes + " is not a valid number" +
-                            "1");
+                        var minutes = double.Parse(entry);
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not a valid number" +
+                                "1");
+                            continue;
+                        }
+
+                        if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go Hot Stuff!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Now you are showing off!");
+                        }
+                        // Add minutes exercised to total
+                        runningTotal += minutes;
+                  
+                        // Display total minutes excercied to the screen
+                        Console.WriteLine("You've exercised for " + entry + " minutes, and have worked out for " + runningTotal);
+                        }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not a valid entry");
                         continue;
                     }
-
-                    if (minutes <= 10)
-                    {
-                        Console.WriteLine("Better than nothing, am I right?");
-                    }
-                    else if (minutes <= 30)
-                    {
-                        Console.WriteLine("Way to go Hot Stuff!");
-                    }
-                    else if (minutes <= 60)
-                    {
-                        Console.WriteLine("You must be a ninja warrior in training!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Now you are showing off!");
-                    }
-                    // Add minutes exercised to total
-                    runningTotal += minutes;
-                  
-                    // Display total minutes excercied to the screen
-                    Console.WriteLine("You've exercised for " + entry + " minutes, and have worked out for " + runningTotal);
+                 
                 }
                 // Repeat until user in finished             
             }
-
-
-
             Console.WriteLine("Goodbye!");
         }
     }
